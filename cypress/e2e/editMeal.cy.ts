acb8274c248c4c8c3bf7c1e3f0e3fb5af3a60d66
Yao Laptop <luyao2493751@gmail.com>
2025-06-09 18:05:34 +0300

    added assertion for the each edited cell

--- cypress/e2e/editMeal.cy.ts
+++ cypress/e2e/editMeal.cy.ts
@@ -2,14 +2,11 @@ import { createMealViaAPI } from "../utils/api/createMealViaAPI";
 import { loginViaAPI } from "../utils/api/loginViaAPI";
 
 describe("Edit Meal", () => {
-
-
-
     it("Edit a meal in the RamenHouse content management system", () => {
-
         function editCell(stringBefore, stringAfter) {
             cy.get('[gui-structure-row=""]')
-                .filter((_, el) => el.innerText.includes(stringBefore)).last()
+                .filter((_, el) => el.innerText.includes(stringBefore))
+                .last()
                 .within(() => {
                     cy.contains(stringBefore).click({ force: true });
                 });
@@ -17,29 +14,37 @@ describe("Edit Meal", () => {
             cy.focused().clear().type(`${stringAfter}{enter}`, { force: true });
             cy.reload();
             cy.wait(1000);
-            cy.contains(stringAfter).should("exist");
+            cy.contains(stringAfter).should("exist")
+                .then(($el: JQuery<HTMLElement>) => {
+                    if ($el) {
+
+                        expect($el.text()).to.include(stringAfter);
+                    }
+                    else {
+                        throw new Error(`Element with text "${stringAfter}" not found`);
+                    }
+                })
         }
 
         const name = "test meal to be edited";
         const description = "This is a test meal that will be edited";
-        const basePrice = 10.00;
+        const basePrice = 10.0;
         const discount = 0.3;
         const isFeatured = false;
 
         const baseUrl = Cypress.env("baseUrl");
-        loginViaAPI()
+        loginViaAPI();
         createMealViaAPI({
             name,
             description,
             BasePrice: basePrice,
             Discount: discount,
-            isFeatured
-        })
+            isFeatured,
+        });
 
         cy.visit(`${baseUrl}admin`);
         cy.wait(1000);
         cy.get("#gui-grid-1").should("exist");
-
 
         // edit the meal's name
         cy.then(() => editCell(name, "[Edited] " + name));
@@ -52,20 +57,12 @@ describe("Edit Meal", () => {
         // edit the meal's isFeatured
         cy.then(() => editCell(isFeatured.toString(), (!isFeatured).toString()));
 
-
         //delete the meal being edited
         cy.get('[gui-structure-row=""]')
-            .filter((_, el) => el.innerText.includes("[Edited] " + name)).last()
+            .filter((_, el) => el.innerText.includes("[Edited] " + name))
+            .last()
             .within(() => {
                 cy.get('[onclick*="deleteMeal"]').click({ force: true });
             });
-
-
-
-
-
-
-
     });
-
 });
